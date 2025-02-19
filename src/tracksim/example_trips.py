ffdfa1445ee071e7006a8f7e8b176e0370bfd795
nawe-aau <nawe@energy.aau.dk>
2025-02-19 13:38:16 +0100

    Fixed example_trips not fetching trip data correctly

--- src/tracksim/example_trips.py
+++ src/tracksim/example_trips.py
@@ -1,6 +1,9 @@
+import os
 import pandas as pd
 
-trip_folder = 'example_trip_data'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+trip_folder = f'{current_dir}/example_trip_data'
 
 udds = pd.read_csv(f'{trip_folder}/udds.csv')
 us06 = pd.read_csv(f'{trip_folder}/us06.csv')
