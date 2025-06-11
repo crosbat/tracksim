a03b07f0448236b1f898586e89f28ecdbbc06503
nawe-aau <nawe@energy.aau.dk>
2025-06-11 17:29:43 +0200

    Added functions to load the datasets

--- src/tracksim/example_trips.py
+++ src/tracksim/example_trips.py
@@ -5,7 +5,18 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 trip_folder = f'{current_dir}/example_trip_data'
 
-udds = pd.read_csv(f'{trip_folder}/udds.csv')
-us06 = pd.read_csv(f'{trip_folder}/us06.csv')
-Weinreich2025_E45_1 = pd.read_csv(f'{trip_folder}/Weinreich2025_E45_1.csv')
-Weinreich2025_E45_2 = pd.read_csv(f'{trip_folder}/Weinreich2025_E45_2.csv')
+def load_udds() -> pd.DataFrame:
+    udds = pd.read_csv(f'{trip_folder}/udds.csv')
+    return udds
+
+def load_us06() -> pd.DataFrame:
+    us06 = pd.read_csv(f'{trip_folder}/us06.csv')
+    return us06
+
+def load_weinreich2025_E45_1() -> pd.DataFrame:    
+    Weinreich2025_E45_1 = pd.read_csv(f'{trip_folder}/Weinreich2025_E45_1.csv')
+    return Weinreich2025_E45_1
+
+def load_weinreich2025_E45_2() -> pd.DataFrame:    
+    Weinreich2025_E45_2 = pd.read_csv(f'{trip_folder}/Weinreich2025_E45_2.csv')
+    return Weinreich2025_E45_2
